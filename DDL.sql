370b25cc770d70907093870a5e657d11011c1523
MayHyeyeonKim <hkim7963@gmail.com>
2024-12-21 01:42:55 -0800

    added cart table

--- DDL.sql
+++ DDL.sql
@@ -131,6 +131,17 @@ AFTER INSERT ON aladinbooks
 FOR EACH ROW
 EXECUTE FUNCTION sync_aladinbooks_to_books();
 
+CREATE TABLE carts (
+  id SERIAL PRIMARY KEY,
+  book_id INTEGER NOT NULL,
+  quantity INTEGER NOT NULL CHECK (quantity >= 1),
+  user_id INTEGER,
+
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+)
+
+
 CREATE TABLE categories
 (
     id BIGINT PRIMARY KEY,
